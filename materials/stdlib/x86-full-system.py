83495e099cba8a599d6f26f16aaa4c63960cfe37
Bobby R. Bruce <bbruce@ucdavis.edu>
2022-06-02 16:16:55 -0700

    Add materials/stdlib/x86-full-system.py
    This is used as a starting point for those creating an X86 Full System
    simulation as part of the stdlib tutorial.

---
+++ materials/stdlib/x86-full-system.py
@@ -0,0 +1,12 @@
+from gem5.utils.requires import requires
+from gem5.components.boards.x86_board import X86Board
+from gem5.components.memory.single_channel import SingleChannelDDR3_1600
+from gem5.components.cachehierarchies.ruby.mesi_two_level_cache_hierarchy import MESITwoLevelCacheHierarchy
+from gem5.components.processors.simple_switchable_processor import SimpleSwitchableProcessor
+from gem5.coherence_protocol import CoherenceProtocol
+from gem5.isas import ISA
+from gem5.components.processors.cpu_types import CPUTypes
+from gem5.resources.resource import Resource
+from gem5.simulate.simulator import Simulator
+from gem5.simulate.exit_event import ExitEvent
+
