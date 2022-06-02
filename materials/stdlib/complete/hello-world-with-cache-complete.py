07843e5bcd284696a7c59c8e60d10dc8cb7b705d
Bobby R. Bruce <bbruce@ucdavis.edu>
2022-06-02 15:51:13 -0700

    Add hello-world-with-cache-complete.py
    Adds materials/stdlib/complete/hello-world-with-cache-complete.py. This
    is a complete version of the "Hello World" script students are asked to
    create as part of the stdlib tutorial.

---
+++ materials/stdlib/complete/hello-world-with-cache-complete.py
@@ -0,0 +1,31 @@
+from gem5.components.boards.simple_board import SimpleBoard
+#from gem5.components.cachehierarchies.classic.no_cache import NoCache
+from gem5.components.cachehierarchies.classic.private_l1_cache_hierarchy import PrivateL1CacheHierarchy
+from gem5.components.memory.single_channel import SingleChannelDDR3_1600
+from gem5.components.processors.simple_processor import SimpleProcessor
+from gem5.components.processors.cpu_types import CPUTypes
+from gem5.resources.resource import Resource
+from gem5.simulate.simulator import Simulator
+
+
+# Obtain the components.
+#cache_hierarchy = NoCache()
+cache_hierarchy = PrivateL1CacheHierarchy(l1d_size="32kB", l1i_size="32kb")
+memory = SingleChannelDDR3_1600("1GiB")
+processor = SimpleProcessor(cpu_type=CPUTypes.ATOMIC, num_cores=1)
+
+#Add them to the board.
+board = SimpleBoard(
+    clk_freq="3GHz",
+    processor=processor,
+    memory=memory,
+    cache_hierarchy=cache_hierarchy,
+)
+
+# Set the workload.
+binary = Resource("x86-hello64-static")
+board.set_se_binary_workload(binary)
+
+# Setup the Simulator and run the simulation.
+simulator = Simulator(board=board)
+simulator.run()
