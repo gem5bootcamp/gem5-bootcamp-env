6c2cf619b637d0ca8dd3be1aa6635e6c186057c0
Jason Lowe-Power <jason@lowepower.com>
2022-06-16 16:50:55 +0000

    materials: Initial code for first developing section
    Signed-off-by: GitHub <noreply@github.com>

---
+++ materials/Developing gem5 models/03-events/complete/my_hello_object.cc
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2017 Jason Lowe-Power
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met: redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer;
+ * redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution;
+ * neither the name of the copyright holders nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "tutorial/my_hello_object.hh"
+
+#include "base/logging.hh"
+#include "base/trace.hh"
+#include "debug/MyHelloExample.hh"
+
+namespace gem5
+{
+
+MyHelloObject::MyHelloObject(const Params &params) :
+    SimObject(params),
+    // This is a C++ lambda. When the event is triggered, it will call the
+    // processEvent() function. (this must be captured)
+    event([this]{ processEvent(); }, name() + ".event"),
+    goodbye(params.goodbye_object),
+    // Note: This is not needed as you can *always* reference this->name()
+    myName(params.name),
+    latency(params.time_to_wait),
+    timesLeft(params.number_of_fires)
+{
+    DPRINTF(MyHelloExample, "Created the hello object\n");
+    panic_if(!goodbye, "MyHelloObject must have a non-null GoodbyeObject");
+}
+
+void
+MyHelloObject::startup()
+{
+    // Before simulation starts, we need to schedule the event
+    schedule(event, latency);
+}
+
+void
+MyHelloObject::processEvent()
+{
+    timesLeft--;
+    DPRINTF(MyHelloExample, "Hello world! Processing the event! %d left\n",
+                          timesLeft);
+
+    if (timesLeft <= 0) {
+        DPRINTF(MyHelloExample, "Done firing!\n");
+        goodbye->sayGoodbye(myName);
+    } else {
+        schedule(event, curTick() + latency);
+    }
+}
+
+} // namespace gem5
---
+++ materials/Developing gem5 models/03-events/my_hello_object.cc
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2017 Jason Lowe-Power
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met: redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer;
+ * redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution;
+ * neither the name of the copyright holders nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "tutorial/my_hello_object.hh"
+
+#include "base/logging.hh"
+#include "base/trace.hh"
+#include "debug/MyHelloExample.hh"
+
+namespace gem5
+{
+
+MyHelloObject::MyHelloObject(const Params &params) :
+    SimObject(params),
+    // Initialize the event
+
+    // Initialize the goodbye object
+
+    // Grab something from params
+
+    // Grab the latency parameter
+
+    // Grab the timesLeft parameter
+{
+    // It's a good idea to have debugging!
+
+    // Make sure to check that the user provides what you expect them to
+}
+
+void
+MyHelloObject::startup()
+{
+    // Before simulation starts, we need to schedule the event
+
+}
+
+void
+MyHelloObject::processEvent()
+{
+    // Count the number of times left
+
+    // if the timesLeft is over, then print "Done firing!"
+    // Otherwise, schedule this event to happen with a certain latency
+}
+
+} // namespace gem5
