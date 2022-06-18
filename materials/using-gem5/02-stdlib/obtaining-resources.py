9d247d3a9bf101c09f892312a539b3e21e4eb40b
Bobby R. Bruce <bbruce@ucdavis.edu>
2022-06-18 06:12:22 -0400

    Fix typo in obtaining-resources.py

--- materials/using-gem5/02-stdlib/obtaining-resources.py
+++ materials/using-gem5/02-stdlib/obtaining-resources.py
@@ -1,5 +1,5 @@
 from gem5.resources.resource import Resource
 
-resources = Resource("riscv-disk-img")
+resource = Resource("riscv-disk-img")
 
 print(f"The resource is available at {resource.get_local_path()}")
