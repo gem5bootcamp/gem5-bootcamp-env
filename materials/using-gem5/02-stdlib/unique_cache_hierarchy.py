2863ff082136ab3164e7a6d6a8e513131c7b9161
Bobby R. Bruce <bbruce@ucdavis.edu>
2022-06-18 07:37:20 -0400

    Add unique_cache_hierarchy.py to materials
    This is used in the stdlib tutorial where students are asked to create
    their own component.

---
+++ materials/using-gem5/02-stdlib/unique_cache_hierarchy.py
@@ -0,0 +1,20 @@
+from gem5.components.cachehierarchies.classic.abstract_classic_cache_hierarchy \
+    import AbstractClassicCacheHierarchy
+from gem5.components.boards.abstract_board import AbstractBoard
+
+from m5.objects import Port
+
+class UniqueCacheHierarchy(AbstractClassicCacheHierarchy):
+
+
+    def __init__() -> None:
+        AbstractClassicCacheHierarchy.__init__(self=self)
+
+    def get_mem_side_port(self) -> Port:
+        pass
+
+    def get_cpu_side_port(self) -> Port:
+        pass
+
+    def incorporate_cache(self, board: AbstractBoard) -> None:
+        pass
