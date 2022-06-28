f8773eda7c8ae4000b4f409bcda51cb3e4ce491f
Hoa Nguyen <hoanguyen@ucdavis.edu>
2022-06-28 12:11:00 -0700

    Run style check
    Signed-off-by: Hoa Nguyen <hoanguyen@ucdavis.edu>

--- materials/using-gem5/03-gem5-basics/02-frequently-used-features/09-seaborn.py
+++ materials/using-gem5/03-gem5-basics/02-frequently-used-features/09-seaborn.py
@@ -2,8 +2,8 @@
 import seaborn as sns
 
 if __name__ == "__main__":
-    x = [k/2 for k in range(8)]
-    y = [j - j**3/6 + j**5/120 for j in x]
+    x = [k / 2 for k in range(8)]
+    y = [j - j**3 / 6 + j**5 / 120 for j in x]
 
     plot_1 = sns.barplot(x=x, y=y)
     fig = plot_1.get_figure()
