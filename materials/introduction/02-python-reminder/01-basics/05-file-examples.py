118c61003ecf0c6951abe3e32849d99969dbd06d
Hoa Nguyen <hoanguyen@ucdavis.edu>
2022-06-28 18:05:59 -0700

    Style check pass
    Signed-off-by: Hoa Nguyen <hoanguyen@ucdavis.edu>

--- materials/introduction/02-python-reminder/01-basics/05-file-examples.py
+++ materials/introduction/02-python-reminder/01-basics/05-file-examples.py
@@ -2,12 +2,14 @@ if __name__ == "__m5_main__":
     # write to a file
     with open("example.txt", "w") as output_file:
         for i in range(10):
-            output_file.write(f"{i}\n") # the write function does not emit "\n" by default
+            output_file.write(
+                f"{i}\n"
+            )  # the write function does not emit "\n" by default
 
     # read from a file
     with open("example.txt", "r") as input_file:
-        for line in input_files.readlines(): # read all lines from the input file
-            line = line.strip() # strip the \n character of each line
+        for line in input_files.readlines():  # read all lines from the input file
+            line = line.strip()  # strip the \n character of each line
             print(line)
 
     # add more content to a file
