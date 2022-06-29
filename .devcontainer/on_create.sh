bce9860833a341a67154cf5733f5458e7e932b86
Jason Lowe-Power <jason@lowepower.com>
2022-06-28 20:14:39 -0700

    Update on_create.sh

--- .devcontainer/on_create.sh
+++ .devcontainer/on_create.sh
@@ -4,8 +4,8 @@
 git submodule update --init --recursive
 
 # Create the stubs
-gem5-arm gem5/util/gem5_stubgen.py
-gem5-x86 gem5/util/gem5_stubgen.py
-gem5-riscv gem5/util/gem5_stubgen.py
+gem5-arm gem5_stubgen.py
+gem5-x86 gem5_stubgen.py
+gem5-riscv gem5_stubgen.py
 
 mv out typings
