5997aeef8f05e05e571bc52e24f7a24756ab0ba6
Hoa Nguyen <hoanguyen@ucdavis.edu>
2022-07-06 11:22:58 -0700

    Update class variable example
    Signed-off-by: Hoa Nguyen <hoanguyen@ucdavis.edu>

--- materials/introduction/02-gem5-basics/02-frequently-used-features/02-class-variables.py
+++ materials/introduction/02-gem5-basics/02-frequently-used-features/02-class-variables.py
@@ -1,69 +1,92 @@
 class Processor:
-    type_name = "Processor"
+    default_frequency = "3GHz"
 
-    def __init__(self, freq):
-        self.freq = freq
+    def __init__(self, name):
+        self.name = name
 
-    def change_type_name(self, new_name):
-        self.type_name = new_name  # this does not change Processor.type_name
-        # it creates an object variable instead
+    def change_default_frequency(self, new_frequency):
+        # this does not change Processor.default_frequency
+        # it creates an *object* variable instead
+        self.default_frequency = new_frequency
+
+    def to_string(self):
+        return str(vars(self))
 
 
 class ProcessorWithClassFunction(Processor):
-    def __init__(self, freq):
-        super(ProcessorWithClassFunction, self).__init__(freq)
+    def __init__(self, name):
+        super().__init__(name)
 
     @classmethod  # A proper way to update a class variable is to use class methods
-    def change_type_name_with_class_function(cls, new_name):
-        cls.type_name = new_name
+    def change_default_frequency_with_class_function(cls, new_frequency):
+        cls.default_frequency = new_frequency
 
 
 if __name__ == "__m5_main__":
-    print("***** Class variable example")
+    cpu1 = Processor("Processor 1")
+    cpu2 = Processor("Processor 2")
 
-    cpu1 = Processor("1GHz")
-    cpu2 = Processor("2GHz")
-
-    print("cpu1 type_name:", cpu1.type_name)
-    print("cpu2 type_name:", cpu2.type_name)
-    print("Processor type_name:", Processor.type_name)
+    print("***** Class variable example 1: Accessing a class variable")
+    print(f"Accessing default_frequency via cpu1: {cpu1.default_frequency}")
+    print(f"Accessing default_frequency via cpu2: {cpu2.default_frequency}")
+    print(f"Accessing default_frequency via Processor: {Processor.default_frequency}")
     print()
 
-    Processor.type_name = "MyProcessor"
-    print("cpu1 type_name:", cpu1.type_name)
-    print("cpu2 type_name:", cpu2.type_name)
-    print("Processor type_name:", Processor.type_name)
+    Processor.default_frequency = "1GHz"
+    print("***** Class variable example 2: Directly changing a class variable")
+    print(f"Accessing default_frequency via cpu1: {cpu1.default_frequency}")
+    print(f"Accessing default_frequency via cpu2: {cpu2.default_frequency}")
+    print(f"Accessing default_frequency via Processor: {Processor.default_frequency}")
     print()
 
-    cpu1.change_type_name("MyProcessor111")
-    print("cpu1 type_name:", cpu1.type_name)
-    print(vars(cpu1))
-    print("cpu2 type_name:", cpu2.type_name)
-    print(vars(cpu2))
-    print("Processor type_name:", Processor.type_name)
+    cpu1.default_frequency = "200MHz"  # this will create an object variable for cpu2 1
+    print("***** Class variable example 3: Mistakenly creating an object variable")
+    print(f"Accessing default_frequency via cpu1: {cpu1.default_frequency}")
+    print(f"Accessing default_frequency via cpu2: {cpu2.default_frequency} (why?)")
+    print(
+        f"Accessing default_frequency via Processor: {Processor.default_frequency} (why?)"
+    )
+    print()
+    print(f"cpu1 object variables: {vars(cpu1)}")
+    print(f"cpu2 object variables: {vars(cpu2)}")
     print()
 
-    cpu2.type_name = "NotProcessor"  # this will create an object variable for cpu2
-    print("cpu1 type_name:", cpu1.type_name)
-    print(vars(cpu1))
-    print("cpu2 type_name:", cpu2.type_name)
-    print(vars(cpu2))
-    print("Processor type_name:", Processor.type_name)
+    cpu2.change_default_frequency("800MHz")
+    print("***** Class variable example 4: Mistakenly creating an object variable")
+    print(f"Accessing default_frequency via cpu1: {cpu1.default_frequency} (why?)")
+    print(f"Accessing default_frequency via cpu2: {cpu2.default_frequency} (why?)")
+    print(
+        f"Accessing default_frequency via Processor: {Processor.default_frequency} (why?)"
+    )
+    print()
+    print(f"cpu1 object variables: {vars(cpu1)}")
+    print(f"cpu2 object variables: {vars(cpu2)}")
     print()
 
-    # change the type_name back to the original one
-    Processor.type_name = "Processor"
+    # change the default_frequency back to the original one
+    Processor.default_frequency = "3GHz"
 
-    print("***** Class function example")
-    cpu1 = ProcessorWithClassFunction("1GHz")
-    cpu2 = ProcessorWithClassFunction("2GHz")
-    ProcessorWithClassFunction.change_type_name_with_class_function("NewProcessor")
-    print("cpu1 type_name:", cpu1.type_name)
-    print("cpu2 type_name:", cpu2.type_name)
-    print("ProcessorWithClassFunction type_name:", ProcessorWithClassFunction.type_name)
+    cpu1 = ProcessorWithClassFunction("ProcessorWithClassFunction 1")
+    cpu2 = ProcessorWithClassFunction("ProcessorWithClassFunction 2")
+
+    print(
+        "***** Class function example 5: Changing a class variable via a class function"
+    )
+    ProcessorWithClassFunction.change_default_frequency_with_class_function("3.5GHz")
+    print(f"Accessing default_frequency via cpu1: {cpu1.default_frequency}")
+    print(f"Accessing default_frequency via cpu2: {cpu2.default_frequency}")
+    print(f"Accessing default_frequency via Processor: {Processor.default_frequency}")
+    print(f"cpu1 object variables: {vars(cpu1)}")
+    print(f"cpu2 object variables: {vars(cpu2)}")
     print()
 
-    cpu1.change_type_name_with_class_function("NewImprovedProcessor")
-    print("cpu1 type_name:", cpu1.type_name)
-    print("cpu2 type_name:", cpu2.type_name)
-    print("ProcessorWithClassFunction type_name:", ProcessorWithClassFunction.type_name)
+    print(
+        "***** Class function example 6: Changing a class variable via a class function"
+    )
+    cpu1.change_default_frequency_with_class_function("4.5GHz")
+    print(f"Accessing default_frequency via cpu1: {cpu1.default_frequency}")
+    print(f"Accessing default_frequency via cpu2: {cpu2.default_frequency}")
+    print(f"Accessing default_frequency via Processor: {Processor.default_frequency}")
+    print(f"cpu1 object variables: {vars(cpu1)}")
+    print(f"cpu2 object variables: {vars(cpu2)}")
+    print()
