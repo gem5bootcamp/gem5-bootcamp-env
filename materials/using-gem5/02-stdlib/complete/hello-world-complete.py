7b46854a381b57bcf35e06f5c52116630465e783
Bobby R. Bruce <bbruce@ucdavis.edu>
2022-07-10 15:09:06 -0400

    Update hello-world-with-cache to use MatrixMulti
    This is just a temporary modification so we can investigate why
    hello-world-with-cache-complete.py simulates more ticks than
    hello-world-complete.py (the latter contains NoCache).
    
    ```
    gem5-x86 materials/using-gem5/02-stdlib/complete/hello-world-complete.py
    gem5-x86 materials/using-gem5/02-stdlib/complete/hello-world-with-cache-complete.py
    ```

--- materials/using-gem5/02-stdlib/complete/hello-world-complete.py
+++ materials/using-gem5/02-stdlib/complete/hello-world-complete.py
@@ -3,7 +3,7 @@ from gem5.components.cachehierarchies.classic.no_cache import NoCache
 from gem5.components.memory.single_channel import SingleChannelDDR3_1600
 from gem5.components.processors.simple_processor import SimpleProcessor
 from gem5.components.processors.cpu_types import CPUTypes
-from gem5.resources.resource import Resource
+from gem5.resources.resource import Resource, CustomResource
 from gem5.simulate.simulator import Simulator
 
 
@@ -18,7 +18,10 @@ board = SimpleBoard(
 )
 
 # Set the workload.
-binary = Resource("x86-hello64-static")
+# binary = Resource("x86-hello64-static")
+binary = CustomResource(
+    "materials/using-gem5/02-stdlib/matrix-multiply/matrix-multiply"
+)
 board.set_se_binary_workload(binary)
 
 # Setup the Simulator and run the simulation.
